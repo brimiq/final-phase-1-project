6b0ce28133eff93504c061523acd68c3825470a6
bradley <fazebrad13@gmail.com>
2025-09-24 13:00:49 +0300

    re did the js to remove the repetetive event listeners

--- project.js
+++ project.js
@@ -1,66 +1,54 @@
 const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-    card.classList.toggle("flipped");
-  });
-});
-const inputName = document.querySelector(".form")
+const inputName = document.querySelector(".form");
 
 inputName.addEventListener("submit", function(event){
-    event.preventDefault
-})
+  event.preventDefault();
+});
 
-let firstCard = null
-let secondCard = null
-let lockBoard = false //when this is true the cards will be locked in place
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false; // prevent extra clicks during unflip delay
 
-function flipCard(){
-    if(lockBoard) 
-        return;//stops the function if two cards dont match preventing the player from flipping more cards during the delay(1 second)
-    if(this===firstCard) //if the card is the same (this referring to the card clicked) end the function
-        return
-    this.classList.add("flipped")   
-    if (firstCard === null) { //storing first card for comparison
-  firstCard = this;
-  return;
+// Attach the main flip function
+cards.forEach(card => card.addEventListener("click", flipCard));
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return; // prevent double-clicking the same card
+
+  this.classList.add("flipped");
+
+  if (firstCard === null) {
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
 }
 
-    secondCard = this //storing the second card
-    checkForMatch()
+function checkForMatch() {
+  const isMatch = firstCard.dataset.value === secondCard.dataset.value;
+  isMatch ? keepCards() : unflipCards();
 }
 
-function checkForMatch(){
-    const isMatch = firstCard.dataset.value === secondCard.dataset.value
-    if(isMatch){
-        keepCards()
-    }else{
-        unflipCard()
-    }
+function keepCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+}
 
-}
-function keepCards(){
-    firstCard.removeEventListener("clicl", flipCard())
-    secondCard.removeEventListener("click", flipCard())
-    resetBoard()
-}
-function unflipCard(){
-       function unflipCards() {
+function unflipCards() {
   lockBoard = true;
 
   setTimeout(() => {
     firstCard.classList.remove("flipped");
     secondCard.classList.remove("flipped");
-
     resetBoard();
-  }, 1000); // 1 second delay
+  }, 1000);
 }
- function resetBoard() {
+
+function resetBoard() {
   [firstCard, secondCard] = [null, null];
   lockBoard = false;
 }
-
-}
-//Will re-arrange function so they are declared at the right points
-
-
